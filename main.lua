23af13db2562ec70c938a6f7957a38884e1e9444
Sn1fferx <rustam.kh.izh@gmail.com>
2024-09-21 21:56:12 +0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,49 @@
+-- переменные --
+local slimeX = 300
+local slimeY = 200
+local moveSpeed = 200
+local state = "idle"
+local rotation = "right"
 
+-- импорт --
+local slimeIdle = love.graphics.newImage("animations/slime/idle/frame1.png")
+local slimeWalk = love.graphics.newImage("animations/slime/walk/frame1.png")
+
+-- логика --
+function love.update(dt)
+    if love.keyboard.isDown("a") then
+        slimeX = slimeX - moveSpeed * dt
+        state = "walking"
+    elseif love.keyboard.isDown("d") then
+        slimeX = slimeX + moveSpeed * dt
+        state = "walking"
+    else
+        state = "idle"
+    end
+end
+
+-- отрисовка --
+function love.draw()
+    if state == "walking" then
+        if love.keyboard.isDown("a") then
+            love.graphics.draw(slimeWalk, slimeX, slimeY, 0, -1, 1)
+            rotation = "left"
+        else
+            love.graphics.draw(slimeWalk, slimeX, slimeY)
+        end
+    else
+        love.graphics.draw(slimeIdle, slimeX, slimeY)
+    end
+    if rotation == "left" them
+    end
+end
+
+-- помощь --
+-- normal
+--love.graphics.draw(image, x, y, rotation, 1, 1) 
+--
+-- mirror
+--love.graphics.draw(image, x, y, rotation, -1, 1) 
+--
+-- flipped
+--love.graphics.draw(image, x, y, rotation, 1, -1) 
